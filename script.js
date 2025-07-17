7e45cfac7faec88bbf8f4e056682c99b0f420cad
SquirrelDave <141869133+SquirrelDave@users.noreply.github.com>
2025-07-17 11:14:24 -0700

    CLI Commit 1

--- script.js
+++ script.js
@@ -11,6 +11,18 @@ class TextProcessor {
     static validate(text) {
         return text.trim().length > 0 && text.length < 100;
     }
+    static evaluateNumber(text) {
+        const trimmed = text.trim();
+        
+        if (!trimmed || isNaN(trimmed)) {
+            return null;
+        }
+
+        return parseInt(trimmed, 10);
+    }
+    static maNumber(number){
+        return number += 69;
+    }
 }
 
 function processText() {
@@ -23,4 +35,14 @@ function processText() {
     } else {
         output.textContent = "Invalid input";
     }
+}
+
+function doMath() {
+    const input = document.getElementById("mathNbr").value;
+    const output = document.getElementById("numbr");
+    const number = TextProcessor.evaluateNumber(input)
+    if (number !== null) {
+        const sum = TextProcessor.maNumber(number);
+        output.textContent = sum;
+    } else { output.textContent = "can't math that"}
 }
